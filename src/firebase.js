24dcd267367dc5f2a79b9c2d3de2ad4df0823be3
deepak852k1 <deepak852k1@gmail.com>
2022-09-13 09:49:14 +0530

    done

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBvsnu7ZeRJFTJ2I2Fn_LmiCvkTT1yJXIw",
-  authDomain: "todo-app-ddeb5.firebaseapp.com",
-  projectId: "todo-app-ddeb5",
-  storageBucket: "todo-app-ddeb5.appspot.com",
-  messagingSenderId: "711265238682",
-  appId: "1:711265238682:web:e4801fb5e9450d138a6923",
-  measurementId: "G-Z9ND74RD5N"
+  apiKey: "AIzaSyB0sfJaFOhFfrYP57cV50Te23NbaDpZ3Og",
+  authDomain: "react-todo-69b02.firebaseapp.com",
+  projectId: "react-todo-69b02",
+  storageBucket: "react-todo-69b02.appspot.com",
+  messagingSenderId: "569648791106",
+  appId: "1:569648791106:web:58d85202fa7d3cf53e77db"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
